e52803224fe2f898da0487f3f96f60f6402e9b5c
akhil-haridas <4khilharidas@gmail.com>
2024-02-04 13:40:30 +0530

    removed unused

--- src/initSentry.js
+++ src/initSentry.js
@@ -6,8 +6,6 @@ export const initSentry = () => {
         dsn: "https://examplePublicKey@o0.ingest.sentry.io/0",
         integrations: [
             new Sentry.BrowserTracing({
-            // See docs for support of different versions of variation of react router
-            // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
             routingInstrumentation: Sentry.reactRouterV6Instrumentation(
                 React.useEffect,
             ),
@@ -25,12 +23,7 @@ export const initSentry = () => {
             user: { id: 42, email: "john.doe@example.com" },
         },
         tracesSampleRate: 1.0,
-
-        // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
         tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-
-        // Capture Replay for 10% of all sessions,
-        // plus for 100% of sessions with an error
         replaysSessionSampleRate: 0.1,
         replaysOnErrorSampleRate: 1.0,
     });
